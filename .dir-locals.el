b692d3ee486026bb1a770bf779ab073b8524f875
Serina Sakurai <mickeysnow1031@gmail.com>
2024-08-27 07:18:04 +0900

    remove .dir-locals.el from tracking

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (my-org-project-name . "rlvm")
-  (compile-command . "scons")
-  (tab-width . 2)
-  (indent-tabs-mode)))
-
