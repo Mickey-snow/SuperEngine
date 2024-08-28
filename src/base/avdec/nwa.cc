fe12bf46b4145ce5e8ce3902f8e2c3f5636ad6c7
Serina Sakurai <mickeysnow1031@gmail.com>
2024-08-28 09:45:05 +0900

    Refactor `NwaDecoder`

--- src/base/avdec/nwa.cc
+++ src/base/avdec/nwa.cc
@@ -27,59 +27,283 @@
 #include <algorithm>
 #include <sstream>
 
-// Constructor definitions
-NwaHQDecoder::NwaHQDecoder(std::string_view data) : sv_data_(data) {
-  ReadHeader();
+// -----------------------------------------------------------------------
+// class NwaHQDecoder
+// -----------------------------------------------------------------------
+class NwaHQDecoder : public NwaDecoderImpl {
+ public:
+  NwaHQDecoder(std::string_view data) : data_(data) {
+    header_ = reinterpret_cast<const NwaHeader*>(data_.data());
+    sample_stream_ = reinterpret_cast<const avsample_s16_t*>(data_.data() +
+                                                             sizeof(NwaHeader));
+    ValidateData();
+  }
+
+  std::vector<avsample_s16_t> DecodeAll() override {
+    const int sample_count = header_->total_sample_count;
+    std::vector<avsample_s16_t> ret(sample_count);
+    std::copy_n(sample_stream_, ret.size(), ret.begin());
+    return ret;
+  }
+
+  std::vector<avsample_s16_t> DecodeNext() override { return DecodeAll(); }
+
+ private:
+  void ValidateData() {
+    std::ostringstream os;
+
+    if (sizeof(NwaHeader) + header_->original_size != data_.size()) {
+      os << "File size mismatch: expected "
+         << (sizeof(NwaHeader) + header_->original_size) << " bytes, but got "
+         << data_.size() << " bytes.\n";
+    }
+    if (header_->unit_count != 0) {
+      os << "Uncompressed NWA should have 0 units, but got "
+         << header_->unit_count << ".\n";
+    }
+    if (header_->total_sample_count * header_->bits_per_sample !=
+        header_->original_size * 8) {
+      os << "Data stream length is insufficient to hold all samples: expected "
+         << (header_->total_sample_count * header_->bits_per_sample) / 8
+         << " bytes, but got " << header_->original_size << " bytes.\n";
+    }
+
+    std::string errorMsg = os.str();
+    if (!errorMsg.empty())
+      throw std::runtime_error(errorMsg);
+  }
+
+  std::string_view data_;
+  const NwaHeader* header_;
+  const avsample_s16_t* sample_stream_;
+};
+
+// -----------------------------------------------------------------------
+// class NwaHQDecoder
+// -----------------------------------------------------------------------
+class NwaCompDecoder : public NwaDecoderImpl {
+ public:
+  NwaCompDecoder(std::string_view data) : data_(data) {
+    header_ = reinterpret_cast<const NwaHeader*>(data_.data());
+    unit_count_ = header_->unit_count;
+    offset_table_ =
+        reinterpret_cast<const int*>(data_.data() + sizeof(NwaHeader));
+    current_unit_ = 0;
+    ValidateData();
+  }
+
+  std::vector<avsample_s16_t> DecodeAll() override {
+    std::vector<avsample_s16_t> ret;
+    ret.reserve(header_->samples_per_unit * header_->unit_count);
+    while (current_unit_ < unit_count_) {
+      auto samples = DecodeNext();
+      ret.insert(ret.end(), samples.begin(), samples.end());
+    }
+    return ret;
+  }
+
+  std::vector<avsample_s16_t> DecodeNext() override {
+    if (current_unit_ >= unit_count_)
+      throw std::logic_error(
+          "DecodeNext() called when no more data is available for decoding.");
+    return DecodeUnit(current_unit_++);
+  }
+
+  bool HasNext() override { return current_unit_ < unit_count_; }
+
+  std::vector<avsample_s16_t> DecodeUnit(int id) {
+    int start_offset = offset_table_[id],
+        unit_size = (id == (unit_count_ - 1))
+                        ? header_->last_unit_packed_size
+                        : (offset_table_[id + 1] - offset_table_[id]);
+    std::string_view unit_data = data_.substr(start_offset, unit_size);
+    BitStream reader(unit_data.data(), unit_data.size());
+
+    int current_sample_count = 0;
+    const int unit_sample_count = (id == header_->unit_count - 1)
+                                      ? header_->last_unit_sample_count
+                                      : header_->samples_per_unit;
+
+    std::vector<avsample_s16_t> samples;
+    samples.reserve(unit_sample_count);
+
+    avsample_s16_t sample[2] = {}, channel = 0;
+    sample[0] = reader.PopAs<avsample_s16_t>(16);
+    if (header_->channel_count == 2)
+      sample[1] = reader.PopAs<avsample_s16_t>(16);
+    const int compression = header_->compression_level;
+
+    auto ReadSignedMagnitude = [](int value, int bits) {
+      bool sign = (value >> (bits - 1)) & 1;
+      int magnitude = value & ((1 << ((bits - 1))) - 1);
+      return (sign ? -1 : 1) * magnitude;
+    };
+    auto AppendSample = [&samples,
+                         &current_sample_count](avsample_s16_t sample) {
+      samples.push_back(sample);
+      ++current_sample_count;
+    };
+
+    while (current_sample_count < unit_sample_count) {
+      if (reader.Position() >= reader.Size()) {
+        std::ostringstream oss;
+        oss << "Data section length mismatch in unit " << id << ": expected "
+            << unit_size << " bytes, but reached end of data.";
+        throw std::runtime_error(oss.str());
+      }
+
+      unsigned int type = reader.Popbits(3);
+      switch (type) {
+        case 0:
+          if (header_->zero_mode_flag) {
+            int zero_count = reader.Popbits(1);
+            if (zero_count == 0b1)
+              zero_count = reader.Popbits(2);
+            if (zero_count == 0b11)
+              zero_count = reader.Popbits(8);
+
+            while (--zero_count)  // generate (zero_count-1) samples here, the
+                                  // last one will be created outside switch
+              AppendSample(sample[channel]);
+          }
+          break;
+
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6: {
+          int bits = compression >= 3 ? 3 + compression : 5 - compression;
+          int shift = compression >= 3 ? 1 + type : 2 + type + compression;
+          int base = ReadSignedMagnitude(reader.Popbits(bits), bits);
+          sample[channel] += base << shift;
+          break;
+        }
+
+        case 7: {
+          bool flag = reader.Popbits(1);
+          if (flag)
+            sample[channel] = 0;
+          else {
+            int bits = compression >= 3 ? 8 : 8 - compression;
+            int shift = compression >= 3 ? 9 : 9 + compression;
+            int base = ReadSignedMagnitude(reader.Popbits(bits), bits);
+            sample[channel] += base << shift;
+          }
+          break;
+        }
+        default:
+          break;
+      }
+
+      AppendSample(sample[channel]);
+      if (header_->channel_count == 2)
+        channel ^= 1;
+    }
+
+    return samples;
+  }
+
+ private:
+  void ValidateData() {
+    std::ostringstream os;
+
+    if (data_.size() != header_->packed_size) {
+      os << "File size mismatch: expected " << header_->packed_size
+         << " bytes, but got " << data_.size() << " bytes.\n";
+    }
+
+    if (header_->bits_per_sample * header_->total_sample_count !=
+        header_->original_size * 8) {
+      os << "Data stream length mismatch: expected "
+         << (header_->total_sample_count * header_->bits_per_sample) / 8
+         << " bytes, but got " << header_->original_size << " bytes.\n";
+    }
+
+    if (header_->unit_count <= 0) {
+      os << "Invalid unit count: got " << header_->unit_count << ".\n";
+    }
+
+    if (header_->samples_per_unit * (header_->unit_count - 1) +
+            header_->last_unit_sample_count !=
+        header_->total_sample_count) {
+      os << "Sample count mismatch: expected " << header_->total_sample_count
+         << " samples, but calculated "
+         << (header_->samples_per_unit * (header_->unit_count - 1) +
+             header_->last_unit_sample_count)
+         << " samples.\n";
+    }
+
+    std::string errorMsg = os.str();
+    if (!errorMsg.empty())
+      throw std::runtime_error(errorMsg);
+  }
+
+  std::string_view data_;
+  const NwaHeader* header_;
+  int unit_count_;
+  const int* offset_table_;
+  int current_unit_;
+};
+
+// -----------------------------------------------------------------------
+// class NwaDecoder
+// -----------------------------------------------------------------------
+NwaDecoder::NwaDecoder(std::string_view data)
+    : data_(data), header_(reinterpret_cast<const NwaHeader*>(data.data())) {
+  ValidateHeader();
+  CreateImplementation();
+}
+NwaDecoder::NwaDecoder(char* data, size_t length)
+    : NwaDecoder(std::string_view(data, length)) {}
+
+AudioData NwaDecoder::DecodeNext() {
+  return AudioData{.spec = GetSpec(), .data = impl_->DecodeNext()};
 }
 
-NwaHQDecoder::NwaHQDecoder(char* data, size_t length) : sv_data_(data, length) {
-  ReadHeader();
+AudioData NwaDecoder::DecodeAll() {
+  return AudioData{.spec = GetSpec(), .data = impl_->DecodeAll()};
 }
 
-// Method definitions
-std::vector<avsample_s16_t> NwaHQDecoder::DecodeAll() {
-  const int sample_count = hdr_->totalSamples;
-  std::vector<avsample_s16_t> ret(sample_count);
-  std::copy_n(stream_, ret.size(), ret.begin());
-  return ret;
+bool NwaDecoder::HasNext() { return impl_->HasNext(); }
+
+AVSpec NwaDecoder::GetSpec() const {
+  return AVSpec{.sample_rate = header_->sample_rate,
+                .sample_format = AV_SAMPLE_FMT::S16,
+                .channel_layout = {.channel_count = header_->channel_count,
+                                   .channel_order = AV_CHANNEL_ORDER::NATIVE}};
 }
 
-void NwaHQDecoder::ReadHeader() {
-  const char* rawdata = sv_data_.data();
-  hdr_ = (NwaHeader*)rawdata;
+void NwaDecoder::ValidateHeader() const {
+  if (data_.size() <= sizeof(NwaHeader)) {
+    throw std::runtime_error(
+        "Invalid NWA data: data size is too small to contain a valid header.");
+  }
 
-  CheckHeader();
-
-  stream_ = (int16_t*)(rawdata + sizeof(NwaHeader));
-}
-
-void NwaHQDecoder::CheckHeader() {
-  bool hasError = false;
   std::ostringstream os;
-
-  if (hdr_->channels != 1 && hdr_->channels != 2) {
-    hasError = true;
-    os << "Expect mono or stereo audio, got " << hdr_->channels
-       << " channels\n";
+  if (header_->channel_count != 1 && header_->channel_count != 2) {
+    os << "Invalid channel count: expected 1 or 2, but got "
+       << header_->channel_count << ".\n";
+  }
+  if (header_->bits_per_sample != 16) {
+    os << "Invalid bit depth: expected 16-bit audio, but got "
+       << header_->bits_per_sample << "-bit.\n";
+  }
+  if (header_->compression_level < -1 || header_->compression_level > 5) {
+    os << "Invalid compression level: " << header_->compression_level
+       << " is not supported.\n";
   }
 
-  if (hdr_->bitsPerSample != 16) {
-    hasError = true;
-    os << "Expect 16 bit audio, got " << hdr_->bitsPerSample << "bit\n";
-  }
+  std::string errorMsg = os.str();
+  if (!errorMsg.empty())
+    throw std::runtime_error(errorMsg);
+}
 
-  if (hdr_->compressionMode != -1) {
-    hasError = true;
-    os << "Current implementation only supports no compression, audio has "
-          "compression level "
-       << hdr_->compressionMode << '\n';
+void NwaDecoder::CreateImplementation() {
+  if (header_->compression_level == -1) {
+    impl_ = std::make_unique<NwaHQDecoder>(data_);
+  } else {
+    impl_ = std::make_unique<NwaCompDecoder>(data_);
   }
-
-  if (sizeof(NwaHeader) + hdr_->origSize != sv_data_.size()) {
-    hasError = true;
-    os << "File size mismatch\n";
-  }
-
-  if (hasError)
-    throw std::runtime_error(os.str());
 }
--- src/base/avdec/nwa.h
+++ src/base/avdec/nwa.h
@@ -26,7 +26,6 @@
 #define BASE_ACODEC_NWA_H_
 
 #include <memory>
-#include <sstream>
 #include <string_view>
 #include <vector>
 
@@ -34,20 +33,21 @@
 #include "utilities/bitstream.h"
 
 struct NwaHeader {
-  int channels : 16;
-  int bitsPerSample : 16;
-  int samplesPerSec : 32;
-  int compressionMode : 32;
-  int zeroMode : 32;
-  int unitCount : 32;
-  int origSize : 32;
-  int packedSize : 32;
-  int totalSamples : 32;
-  int samplePerUnit : 32;
-  int lastUnitSamples : 32;
-  int lastUnitPackedSize : 32;
+  uint16_t channel_count;
+  uint16_t bits_per_sample;
+  uint32_t sample_rate;
+  int32_t compression_level;
+  uint32_t zero_mode_flag;
+  uint32_t unit_count;
+  uint32_t original_size;
+  uint32_t packed_size;
+  uint32_t total_sample_count;
+  uint32_t samples_per_unit;
+  uint32_t last_unit_sample_count;
+  uint32_t last_unit_packed_size;
 };
 
+// Base class for NWA Decoder implementations
 class NwaDecoderImpl {
  public:
   virtual ~NwaDecoderImpl() = default;
@@ -57,173 +57,27 @@ class NwaDecoderImpl {
   virtual std::vector<avsample_s16_t> DecodeAll() = 0;
 };
 
-class NwaHQDecoder : public NwaDecoderImpl {
- public:
-  NwaHQDecoder(std::string_view data);
-  NwaHQDecoder(char* data, size_t length);
-  ~NwaHQDecoder() = default;
-
-  std::vector<avsample_s16_t> DecodeAll() override;
-  std::vector<avsample_s16_t> DecodeNext() override { return DecodeAll(); }
-
- private:
-  void ReadHeader();
-  void CheckHeader();
-
-  std::string_view sv_data_;
-  NwaHeader* hdr_;
-  int16_t* stream_;
-};
-
-class NwaCompDecoder : public NwaDecoderImpl {
- public:
-  NwaCompDecoder(std::string_view data) : data_(data) {
-    hdr_ = (NwaHeader*)data_.data();
-    offset_count_ = hdr_->unitCount;
-    offset_table_ = (int*)(data_.data() + sizeof(NwaHeader));
-    current_unit_ = 0;
-  }
-  NwaCompDecoder(const char* data, size_t size)
-      : NwaCompDecoder(std::string_view(data, size)) {}
-
-  std::vector<avsample_s16_t> DecodeAll() override {
-    std::vector<avsample_s16_t> ret;
-    ret.reserve(hdr_->samplePerUnit * hdr_->unitCount);
-    while (current_unit_ < offset_count_) {
-      auto samples = DecodeNext();
-      ret.insert(ret.end(), samples.begin(), samples.end());
-    }
-    return ret;
-  }
-
-  std::vector<avsample_s16_t> DecodeNext() override {
-    if (current_unit_ >= offset_count_)
-      throw std::logic_error(
-          "DecodeNext() called when no more data is available for decoding.");
-    return DecodeUnit(current_unit_++);
-  }
-  std::vector<avsample_s16_t> DecodeUnit(int id) {
-    int begin_pos = offset_table_[id],
-        unit_size = id == (offset_count_ - 1)
-                        ? hdr_->lastUnitPackedSize
-                        : (offset_table_[id + 1] - offset_table_[id]);
-    std::string_view unit_data = data_.substr(begin_pos, unit_size);
-    BitStream reader(unit_data.data(), unit_data.size());
-
-    int current_sample_count = 0;
-    const int unit_sample_count =
-        id == hdr_->unitCount - 1 ? hdr_->lastUnitSamples : hdr_->samplePerUnit;
-
-    std::vector<avsample_s16_t> ret;
-    ret.reserve(unit_sample_count);
-    auto yieldSample = [&ret, &current_sample_count](int16_t sample) {
-      ret.push_back(sample);
-      ++current_sample_count;
-    };
-
-    int16_t sample[2] = {}, channel = 0;
-    sample[0] = reader.PopAs<int16_t>(16);
-    if (hdr_->channels == 2)
-      sample[1] = reader.PopAs<int16_t>(16);
-    const int comp = hdr_->compressionMode;
-
-    auto ReadSM = [](int value, int bits) {
-      bool sign = (value >> (bits - 1)) & 1;
-      int magnitude = value & ((1 << ((bits - 1))) - 1);
-      return (sign ? -1 : 1) * magnitude;
-    };
-
-    while (current_sample_count < unit_sample_count) {
-      if (reader.Position() >= reader.Size()) {
-        std::ostringstream oss;
-        oss << "Data section length mismatch. (section id " << id;
-        oss << ", from=" << begin_pos;
-        oss << " size=" << unit_size;
-        throw std::runtime_error(oss.str());
-      }
-
-      unsigned int type = reader.Popbits(3);
-      do {
-        if (type == 0) {
-          if (hdr_->zeroMode) {
-            int zeros = reader.Popbits(1);
-            if (zeros == 0b1)
-              zeros = reader.Popbits(2);
-            if (zeros == 0b11)
-              zeros = reader.Popbits(8);
-
-            while (zeros--)
-              yieldSample(sample[channel]);
-            break;
-          }
-        } else if (1 <= type && type <= 6) {
-          int bits = comp >= 3 ? 3 + comp : 5 - comp;
-          int shift = comp >= 3 ? 1 + type : 2 + type + comp;
-          int base = ReadSM(reader.Popbits(bits), bits);
-          sample[channel] += base << shift;
-        } else if (type == 7) {
-          bool flag = reader.Popbits(1);
-          if (flag)
-            sample[channel] = 0;
-          else {
-            int bits = comp >= 3 ? 8 : 8 - comp;
-            int shift = comp >= 3 ? 9 : 9 + comp;
-            int base = ReadSM(reader.Popbits(bits), bits);
-            sample[channel] += base << shift;
-          }
-        }
-
-        yieldSample(sample[channel]);
-      } while (false);
-
-      if (hdr_->channels == 2)
-        channel ^= 1;
-    }
-
-    return ret;
-  }
-
- private:
-  std::string_view data_;
-  NwaHeader* hdr_;
-  int offset_count_;
-  int* offset_table_;
-  int current_unit_;
-};
-
+// Main NWA Decoder class
 class NwaDecoder {
  public:
-  NwaDecoder(std::string_view data)
-      : data_(data), hdr_((NwaHeader*)data.data()) {
-    if (hdr_->compressionMode == -1)
-      impl_ = std::make_unique<NwaHQDecoder>(data_);
-    else
-      impl_ = std::make_unique<NwaCompDecoder>(data_);
-  }
+  NwaDecoder(std::string_view data);
+  NwaDecoder(char* data, size_t length);
 
-  NwaDecoder(char* data, size_t length)
-      : NwaDecoder(std::string_view(data, length)) {}
+  AudioData DecodeNext();
 
-  AudioData DecodeNext() {
-    return AudioData{.spec = GetSpec(), .data = impl_->DecodeNext()};
-  }
-  AudioData DecodeAll() {
-    return AudioData{.spec = GetSpec(), .data = impl_->DecodeAll()};
-  }
-  bool HasNext() { return impl_->HasNext(); }
+  AudioData DecodeAll();
 
-  AVSpec GetSpec() const {
-    return AVSpec{
-        .sample_rate = hdr_->samplesPerSec,
-        .sample_format = AV_SAMPLE_FMT::S16,
-        .channel_layout = {.channel_count = hdr_->channels,
-                           .channel_order = AV_CHANNEL_ORDER::NATIVE}};
-  }
+  bool HasNext();
+
+  AVSpec GetSpec() const;
 
  private:
   std::string_view data_;
-  const NwaHeader* hdr_;
+  const NwaHeader* header_;
   std::unique_ptr<NwaDecoderImpl> impl_;
+
+  void ValidateHeader() const;
+  void CreateImplementation();
 };
 
 #endif
