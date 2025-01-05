3de664497912c231ac39b5f588cfd3c73d42ee4f
Serina Sakurai <mickeysnow1031@gmail.com>
2025-01-05 18:25:20 +0900

    Define struct `ExprAST` as storage type for a expression AST

---
+++ src/base/expr_ast.cpp
@@ -0,0 +1,92 @@
+// -----------------------------------------------------------------------
+//
+// This file is part of RLVM, a RealLive virtual machine clone.
+//
+// -----------------------------------------------------------------------
+//
+// Copyright (C) 2025 Serina Sakurai
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// -----------------------------------------------------------------------
+
+#include "base/expr_ast.hpp"
+
+#include <format>
+#include <unordered_map>
+
+std::string ToString(Op op) {
+  static const std::unordered_map<Op, std::string> opstr_table{
+      {Op::Add, "+"},
+      {Op::Sub, "-"},
+      {Op::Mul, "*"},
+      {Op::Div, "/"},
+      {Op::Mod, "%"},
+      {Op::BitAnd, "&"},
+      {Op::BitOr, "|"},
+      {Op::BitXor, "^"},
+      {Op::ShiftLeft, "<<"},
+      {Op::ShiftRight, ">>"},
+      {Op::Tilde, "~"},
+      {Op::AddAssign, "+="},
+      {Op::SubAssign, "-="},
+      {Op::MulAssign, "*="},
+      {Op::DivAssign, "/="},
+      {Op::ModAssign, "%="},
+      {Op::BitAndAssign, "&="},
+      {Op::BitOrAssign, "|="},
+      {Op::BitXorAssign, "^="},
+      {Op::ShiftLeftAssign, "<<="},
+      {Op::ShiftRightAssign, ">>="},
+      {Op::Assign, "="},
+      {Op::Equal, "=="},
+      {Op::NotEqual, "!="},
+      {Op::LessEqual, "<="},
+      {Op::Less, "<"},
+      {Op::GreaterEqual, ">="},
+      {Op::Greater, ">"},
+      {Op::LogicalAnd, "&&"},
+      {Op::LogicalOr, "||"}};
+
+  if (opstr_table.contains(op))
+    return opstr_table.at(op);
+  else
+    return std::format("<op:{}>", static_cast<int>(op));
+}
+
+std::string BinaryExpr::DebugString() const {
+  return lhs->DebugString() + ToString(op) + rhs->DebugString();
+}
+
+std::string UnaryExpr::DebugString() const {
+  return ToString(op) + sub->DebugString();
+}
+
+std::string ParenExpr::DebugString() const {
+  return '(' + sub->DebugString() + ')';
+}
+
+std::string ExprAST::DebugString() const {
+  return this->Visit([](const auto& x) -> std::string {
+    using T = std::decay_t<decltype(x)>;
+
+    if constexpr (std::same_as<T, int>)
+      return std::to_string(x);
+    else if constexpr (std::same_as<T, std::string>)
+      return x;
+    else
+      return x.DebugString();
+  });
+}
---
+++ src/base/expr_ast.hpp
@@ -0,0 +1,135 @@
+// -----------------------------------------------------------------------
+//
+// This file is part of RLVM, a RealLive virtual machine clone.
+//
+// -----------------------------------------------------------------------
+//
+// Copyright (C) 2025 Serina Sakurai
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// -----------------------------------------------------------------------
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <variant>
+#include <vector>
+
+class ExprAST;
+
+// Expression operator
+enum Op : char {
+  // Arithmetic Operators
+  Add,  // "+"
+  Sub,  // "-"
+  Mul,  // "*"
+  Div,  // "/"
+  Mod,  // "%"
+
+  // Bitwise Operators
+  BitAnd,      // "&"
+  BitOr,       // "|"
+  BitXor,      // "^"
+  ShiftLeft,   // "<<"
+  ShiftRight,  // ">>"
+  Tilde,       // "~"
+
+  // Compound Assignment Operators
+  AddAssign,         // "+="
+  SubAssign,         // "-="
+  MulAssign,         // "*="
+  DivAssign,         // "/="
+  ModAssign,         // "%="
+  BitAndAssign,      // "&="
+  BitOrAssign,       // "|="
+  BitXorAssign,      // "^="
+  ShiftLeftAssign,   // "<<="
+  ShiftRightAssign,  // ">>="
+
+  // Assignment Operator
+  Assign,  // "="
+
+  // Comparison Operators
+  Equal,         // "=="
+  NotEqual,      // "!="
+  LessEqual,     // "<="
+  Less,          // "<"
+  GreaterEqual,  // ">="
+  Greater,       // ">"
+
+  // Logical Operators
+  LogicalAnd,  // "&&"
+  LogicalOr    // "||"
+};
+std::string ToString(Op op);
+
+// Binary operation node
+struct BinaryExpr {
+  Op op;
+  std::unique_ptr<ExprAST> lhs;
+  std::unique_ptr<ExprAST> rhs;
+
+  std::string DebugString() const;
+};
+
+// Unary operation node
+struct UnaryExpr {
+  Op op;
+  std::unique_ptr<ExprAST> sub;
+
+  std::string DebugString() const;
+};
+
+// Parenthesized expression node
+struct ParenExpr {
+  std::unique_ptr<ExprAST> sub;
+
+  std::string DebugString() const;
+};
+
+using expr_variant_t = std::variant<int,          // integer literal
+                                    std::string,  // identifier
+                                    BinaryExpr,
+                                    UnaryExpr,
+                                    ParenExpr>;
+
+class ExprAST {
+ public:
+  ExprAST(expr_variant_t var) : var_(std::move(var)) {}
+
+  std::string DebugString() const;
+
+  template <class Visitor>
+  decltype(auto) Visit(Visitor&& vis) {
+    return std::visit(std::forward<Visitor>(vis), var_);
+  }
+  template <class Visitor>
+  decltype(auto) Visit(Visitor&& vis) const {
+    return std::visit(std::forward<Visitor>(vis), var_);
+  }
+  template <class R, class Visitor>
+  R Visit(Visitor&& vis) {
+    return std::visit<R>(std::forward<Visitor>(vis), var_);
+  }
+  template <class R, class Visitor>
+  R Visit(Visitor&& vis) const {
+    return std::visit<R>(std::forward<Visitor>(vis), var_);
+  }
+
+ private:
+  expr_variant_t var_;
+};
