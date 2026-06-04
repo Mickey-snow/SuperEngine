11343ab6a591ea99c4a6ab13c07a3ad606667f40
Serina Sakurai <mickeysnow1031@gmail.com>
2026-06-04 21:20:42 +0900

    siglus: Implement pcmch

--- src/libsiglus/bindings/sound.cpp
+++ src/libsiglus/bindings/sound.cpp
@@ -22,13 +22,16 @@
 // -----------------------------------------------------------------------
 
 #include "libsiglus/bindings/registry.hpp"
+#include "libsiglus/bindings/util.hpp"
 #include "srbind/srbind.hpp"
 #include "systems/sound_system.hpp"
 #include "systems/system.hpp"
 #include "vm/string.hpp"
 #include "vm/vm.hpp"
 
+#include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace libsiglus::binding {
@@ -164,6 +167,152 @@ class SiglusBgm {
   std::string registered_name_;
 };
 
+class SiglusPcmch {
+ public:
+  SiglusPcmch(System* sys, int channel) : system_(sys), channel_(channel) {}
+
+  void play(std::vector<sr::Value> args) {
+    Play(std::move(args), false, false, false);
+  }
+
+  void play_loop(std::vector<sr::Value> args) {
+    Play(std::move(args), true, false, false);
+  }
+
+  void play_wait(std::vector<sr::Value> args) {
+    Play(std::move(args), false, true, false);
+  }
+
+  void ready(std::vector<sr::Value> args) {
+    Play(std::move(args), false, false, true);
+  }
+
+  void ready_loop(std::vector<sr::Value> args) {
+    Play(std::move(args), true, false, true);
+  }
+
+  void stop(std::vector<sr::Value> args) {
+    const int fade_ms = args.empty() ? 0 : AsInt(args[0]).value_or(0);
+    if (system_ && IsValidChannel()) {
+      if (fade_ms > 0)
+        system_->sound().WavFadeOut(channel_, fade_ms);
+      else
+        system_->sound().WavStop(channel_);
+    }
+    state_ = fade_ms > 0 ? kFadeOut : kFree;
+    loop_ = false;
+  }
+
+  void pause(std::vector<sr::Value>) { state_ = kPause; }
+
+  void resume(std::vector<sr::Value> args) {
+    const int fade_ms = args.empty() ? 0 : AsInt(args[0]).value_or(0);
+    ready_ = false;
+    if (!pcm_name_.empty())
+      StartPlayback(fade_ms);
+    state_ = kPlay;
+  }
+
+  void resume_wait(std::vector<sr::Value> args) {
+    resume(std::move(args));
+    wait({});
+  }
+
+  // TODO: Implement Wait
+  void wait(std::vector<sr::Value>) {}
+  int wait_key(std::vector<sr::Value>) { return 0; }
+  void wait_fade(std::vector<sr::Value>) { state_ = kFree; }
+  int wait_fade_key(std::vector<sr::Value>) {
+    state_ = kFree;
+    return 0;
+  }
+
+  int check(std::vector<sr::Value>) const {
+    if (state_ == kFadeOut || state_ == kPause)
+      return state_;
+    if (system_ && IsValidChannel() && system_->sound().WavPlaying(channel_))
+      return kPlay;
+    return state_ == kPlay ? kPlay : kFree;
+  }
+
+  void set_volume(std::vector<sr::Value> args) {
+    if (args.empty())
+      return;
+
+    const int fade_ms = args.size() > 1 ? AsInt(args[1]).value_or(0) : 0;
+    SetVolume(AsInt(args[0]).value_or(volume_), fade_ms);
+  }
+
+  void set_vol_max(std::vector<sr::Value> args) {
+    const int fade_ms = args.empty() ? 0 : AsInt(args[0]).value_or(0);
+    SetVolume(kVolumeMax, fade_ms);
+  }
+
+  void set_vol_min(std::vector<sr::Value> args) {
+    const int fade_ms = args.empty() ? 0 : AsInt(args[0]).value_or(0);
+    SetVolume(kVolumeMin, fade_ms);
+  }
+
+  int get_volume(std::vector<sr::Value>) const { return volume_; }
+
+ private:
+  void Play(std::vector<sr::Value> args, bool loop, bool wait, bool ready) {
+    if (args.empty())
+      return;
+
+    pcm_name_ = AsString(args[0]);
+    fade_in_ms_ = args.size() > 1 ? AsInt(args[1]).value_or(0) : 0;
+    loop_ = loop;
+    ready_ = ready;
+
+    if (ready) {
+      state_ = kFree;
+      return;
+    }
+
+    StartPlayback(fade_in_ms_);
+    if (wait)
+      this->wait({});
+  }
+
+  void StartPlayback(int fade_ms) {
+    state_ = kPlay;
+    if (system_ && IsValidChannel() && !pcm_name_.empty())
+      system_->sound().WavPlay(pcm_name_, loop_, channel_, fade_ms);
+  }
+
+  void SetVolume(int volume, int fade_ms) {
+    volume_ = volume;
+    if (!system_ || !IsValidChannel())
+      return;
+
+    if (fade_ms > 0)
+      system_->sound().SetChannelVolume(channel_, volume_, fade_ms);
+    else
+      system_->sound().SetChannelVolume(channel_, volume_);
+  }
+
+  bool IsValidChannel() const {
+    return channel_ >= 0 && channel_ < NUM_TOTAL_CHANNELS;
+  }
+
+  static constexpr int kFree = 0;
+  static constexpr int kPlay = 1;
+  static constexpr int kFadeOut = 2;
+  static constexpr int kPause = 3;
+  static constexpr int kVolumeMin = 0;
+  static constexpr int kVolumeMax = 255;
+
+  System* system_;
+  int channel_;
+  int state_ = kFree;
+  int volume_ = kVolumeMax;
+  int fade_in_ms_ = 0;
+  bool loop_ = false;
+  bool ready_ = false;
+  std::string pcm_name_;
+};
+
 void BindSound(Context&, SiglusRuntime& runtime) {
   sr::VM& vm = *runtime.vm;
 
@@ -189,6 +338,45 @@ void BindSound(Context&, SiglusRuntime& runtime) {
   bgm.def("get_volume", &SiglusBgm::get_volume, sb::vararg);
   bgm.def("get_regist_name", &SiglusBgm::get_regist_name, sb::vararg);
   bgm.def("get_play_pos", &SiglusBgm::get_play_pos, sb::vararg);
+
+  sb::class_<SiglusPcmch> pcmch(m, "__SiglusPcmch");
+  pcmch.def(sb::init([sys = runtime.system.get()](int channel) {
+              return new SiglusPcmch(sys, channel);
+            }),
+            sb::arg("channel"));
+  pcmch.def("play", &SiglusPcmch::play, sb::vararg);
+  pcmch.def("play_loop", &SiglusPcmch::play_loop, sb::vararg);
+  pcmch.def("play_wait", &SiglusPcmch::play_wait, sb::vararg);
+  pcmch.def("ready", &SiglusPcmch::ready, sb::vararg);
+  pcmch.def("ready_loop", &SiglusPcmch::ready_loop, sb::vararg);
+  pcmch.def("stop", &SiglusPcmch::stop, sb::vararg);
+  pcmch.def("pause", &SiglusPcmch::pause, sb::vararg);
+  pcmch.def("resume", &SiglusPcmch::resume, sb::vararg);
+  pcmch.def("resume_wait", &SiglusPcmch::resume_wait, sb::vararg);
+  pcmch.def("wait", &SiglusPcmch::wait, sb::vararg);
+  pcmch.def("wait_key", &SiglusPcmch::wait_key, sb::vararg);
+  pcmch.def("wait_fade", &SiglusPcmch::wait_fade, sb::vararg);
+  pcmch.def("wait_fade_key", &SiglusPcmch::wait_fade_key, sb::vararg);
+  pcmch.def("check", &SiglusPcmch::check, sb::vararg);
+  pcmch.def("set_volume", &SiglusPcmch::set_volume, sb::vararg);
+  pcmch.def("set_vol_max", &SiglusPcmch::set_vol_max, sb::vararg);
+  pcmch.def("set_vol_min", &SiglusPcmch::set_vol_min, sb::vararg);
+  pcmch.def("get_volume", &SiglusPcmch::get_volume, sb::vararg);
+
+  Execute(vm, R"(
+class __SiglusPcmchList {
+  fn __init__(self){
+    self.storage = [];
+  }
+  fn __getitem__(self, idx){
+    while(self.storage.len() <= idx) self.storage.append(nil);
+    if(self.storage[idx] == nil) self.storage[idx] = __SiglusPcmch(idx);
+    return self.storage[idx];
+  }
+}
+
+pcmch_list = __SiglusPcmchList();
+)");
 }
 
 RLVM_REGISTER(SiglusBindingRegistry, "sound", BindSound)
